bb223b410d691ec50dbcfcb6fbf1f067bb749862
Mian King <Bilal Jan>
2021-09-03 10:56:30 +0200

    prod keys test

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-if (process.env.NODE_ENV === ' production') {
-    // we are in production - return prod set of keys
+if (process.env.NODE_ENV === 'production') {
+    // we are in production - return the prod set of keys
     module.exports = require('./prod');
 } else {
-    // we are in develpment - return dev keys
+    // we are in development - return the dev keys!!!
     module.exports = require('./dev');
 }
--- config/prod.js
+++ config/prod.js
@@ -3,4 +3,4 @@ module.exports = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
-}
+};
