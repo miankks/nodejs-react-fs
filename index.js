671e00a1180e9332fec83e0843ca56ae1ed41df1
Mian King <Bilal Jan>
2021-09-11 11:15:25 +0200

    sendgrid setup mailer properties and model

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 // const authRoutes = require('./routes/authRoutes');   // be done writing directly with app methid
 
@@ -27,6 +28,7 @@ app.use(passport.session());
 // authRoutes(app);
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
---
+++ services/Mailer.js
@@ -0,0 +1,42 @@
+const sendGrid = require('sendgrid');
+const helper = sendGrid.mail;
+const keys = require('../config/keys');
+
+class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super();
+
+        this.from_email = new helper.Email('no-reply@bootcamp.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddressses(recipients);
+
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
+    }
+
+    formatAddressses(recipients) {
+        return recipients.map(({ email }) => {
+            return new helper.Email(email);
+        })
+    }
+
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        this.recipients.forEach( recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
+}
+
+module.exports = Mailer;
