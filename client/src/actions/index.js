b7382695aed7f03b50813b78b8a4f5e5e7eebc53
Mian King <Bilal Jan>
2021-09-19 18:07:49 +0200

    refactor

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,6 +12,18 @@ export const handleToken = (token) => async dispatch => {
     dispatch({ type: FETCH_USER, payload: res.data});
 }
 
+export const submitSurvey = (values, history) => async dispatch => {
+    const res = await axios.post('/api/surveys', values)
+
+    history.push('/surveys');
+    dispatch(
+        {
+           type: 'FETCH_USER',
+           payload: res.data
+       }
+    )
+}
+
     // old style
 
     // export const fetchUser = () => {
